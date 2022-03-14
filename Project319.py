98f46f4281d0850c8ded06f1fb43145782b17db8
Adi2311 <98967485+Adi2311@users.noreply.github.com>
2022-03-14 22:56:27 +0530

    Add files via upload
    - Implemented Feature extraction for all except ACLowEnergy

--- Project319.py
+++ Project319.py
@@ -1,4 +1,4 @@
-from scipy import signal
+from scipy import signal, fft
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,6 +13,7 @@ a_transposed = np.transpose(a[0])
 
 out = np.empty(shape = [6,155])
 Final_out = np.empty(shape = [5850,155,6])
+Ftr = np.empty(shape = [5850,9,6])
 
 #process the data
 for y in range(5850):
@@ -30,6 +31,8 @@ for y in range(5850):
 
         sos = signal.butter(10, 0.12903225806, 'lp', output='sos')
         filtered = signal.sosfilt(sos, sig)
+        transformed = fft.fft(filtered)
+        transformed[72:] = 0
         #ax2.plot(t, filtered)
         #ax2.set_title('After 2hz hp filter with sampling frequency =1000hz')
         #ax2.axis([0, 1, -1.5, 3])
@@ -37,7 +40,17 @@ for y in range(5850):
         #plt.tight_layout()
         #plt.show()
 
-        out[x] = filtered
+        out[x] = transformed
+
+        #   Feature Extraction
+
+        Ftr[y,0,x] = sum(np.square(transformed))
+        Ftr[y,2,x] = sum(sig)
+        Ftr[y,3,x] = np.mean(sig)
+        Ftr[y,6,x] = sum(abs(sig))
+        Ftr[y,7,x] = np.mean(abs(sig))
+        
+               
         #print(sig.shape)
         #print(out.shape)
         #print(out[x].shape)
@@ -48,9 +61,27 @@ for y in range(5850):
 
         # remember to change the paths of input 
 
+    # More Feature extraction
+
+    Ftr[y,4,0:3] =(Ftr[y,3,0] + Ftr[y,3,1] + Ftr[y,3,2])/3
+    Ftr[y,4,3:6] = (Ftr[y,3,3] + Ftr[y,3,4] + Ftr[y,3,5])/3
+
+    Ftr[y,5,0] = Ftr[y,3,1] - Ftr[y,3,0]
+    Ftr[y,5,1] = Ftr[y,3,2] - Ftr[y,3,0]
+    Ftr[y,5,2] = Ftr[y,3,2] - Ftr[y,3,1]
+    Ftr[y,5,3] = Ftr[y,3,4] - Ftr[y,3,3]
+    Ftr[y,5,4] = Ftr[y,3,5] - Ftr[y,3,3]
+    Ftr[y,5,5] = Ftr[y,3,5] - Ftr[y,3,4] 
+
+    Ftr[y,8,0:3] = (Ftr[y,6,0] + Ftr[y,6,1] + Ftr[y,6,2])
+    Ftr[y,8,3:6] = (Ftr[y,6,3] + Ftr[y,6,4] + Ftr[y,6,5])
+
+    
     op = np.transpose(out)
     #print(op)
     Final_out[y] = op
     #print(Final_out[0])
     
 np.save("X_Test_Processed.npy",Final_out)
+np.save("Feature.npy",Ftr)
+
